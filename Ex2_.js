71348828059dd85bc6ff99b2f48d128d7e70cb2a
Charlee2543 <Charlee2543@hotmail.com>
2024-05-16 14:02:24 +0700

    Ex2_Splice

---
+++ Ex2_.js
@@ -0,0 +1,9 @@
+// #### Part 1
+const orderName = ["Sofia","David","Juan"];
+// #### Part 2
+orderName.push("Sara","Augustin");
+// #### Part 3
+orderName.splice(2,0,"Renata");
+orderName.push("Elena");
+
+console.log(orderName);
